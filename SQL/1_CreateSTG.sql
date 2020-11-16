6b6368f80ecda498dcda4edb2c5b2b10e8daf09a
ignaczg <ignaczg@gmail.com>
2020-11-17 00:19:58 +0100

    order correction

--- SQL/1_CreateSTG.sql
+++ SQL/1_CreateSTG.sql
@@ -17,77 +17,77 @@ DROP TABLE IF EXISTS A_STG.dbo.STG_stylecategories
 DROP TABLE IF EXISTS A_STG.dbo.STG_styles
 
 CREATE TABLE A_STG.dbo.STG_beeravaliabilities(
-availability_category VARCHAR(256),
-availability_id VARCHAR(256))
+availability_id VARCHAR(256),
+availability_category VARCHAR(256))
 
 CREATE TABLE A_STG.dbo.STG_beers(
-abv VARCHAR(256),
-availability_type VARCHAR(256),
 beer_id VARCHAR(256),
 beer_name VARCHAR(256),
 brewery_id VARCHAR(256),
 location_id VARCHAR(256),
-notes VARCHAR(8192),
-retired VARCHAR(256),
-style_id VARCHAR(256))
+style_id VARCHAR(256),
+availability_type VARCHAR(256),
+abv VARCHAR(256),
+notes VARCHAR(4000),
+retired VARCHAR(256))
 
 CREATE TABLE A_STG.dbo.STG_breweries(
 brewery_id VARCHAR(256),
 brewery_name VARCHAR(256),
 location_id VARCHAR(256),
-note VARCHAR(8192))
+note VARCHAR(4000))
 
 CREATE TABLE A_STG.dbo.STG_breweryservices(
 brewery_id VARCHAR(256),
 serving_type VARCHAR(256))
+
 CREATE TABLE A_STG.dbo.STG_costumers(
-email VARCHAR(256),
+usr_name VARCHAR(256),
 first_name VARCHAR(256),
 last_name VARCHAR(256),
-location_id VARCHAR(256),
-usr_name VARCHAR(256))
+email VARCHAR(256),
+location_id VARCHAR(256))
 
 CREATE TABLE A_STG.dbo.STG_countries(
 country_code VARCHAR(256),
 country_name VARCHAR(256),
-intermediate_region VARCHAR(256),
 region VARCHAR(256),
-sub_region VARCHAR(256))
+sub_region VARCHAR(256),
+intermediate_region VARCHAR(256))
 
 CREATE TABLE A_STG.dbo.STG_locations(
-city VARCHAR(256),
-country_code VARCHAR(256),
 location_id VARCHAR(256),
-stat VARCHAR(256))
+city VARCHAR(256),
+stat VARCHAR(256),
+country_code VARCHAR(256))
 
 CREATE TABLE A_STG.dbo.STG_reviews(
 beer_id VARCHAR(256),
-feel VARCHAR(256),
-look VARCHAR(256),
-overall VARCHAR(256),
+usr_name VARCHAR(256),
 review_date VARCHAR(256),
-score VARCHAR(256),
+look VARCHAR(256),
 smell VARCHAR(256),
 taste VARCHAR(256),
-usr_name VARCHAR(256))
+feel VARCHAR(256),
+overall VARCHAR(256),
+score VARCHAR(256))
 
 CREATE TABLE A_STG.dbo.STG_servingtypes(
-serving_category VARCHAR(256),
-serving_id VARCHAR(256))
+serving_id VARCHAR(256),
+serving_category VARCHAR(256))
 
 CREATE TABLE A_STG.dbo.STG_stylecategories(
 category_id VARCHAR(256),
 style_category VARCHAR(256))
 
 CREATE TABLE A_STG.dbo.STG_styles(
-abv_high VARCHAR(256),
-abv_low VARCHAR(256),
-category_type VARCHAR(256),
-co2_volume_high VARCHAR(256),
-co2_volume_low VARCHAR(256),
-ibu_high VARCHAR(256),
-ibu_lower VARCHAR(256),
-lambicdegree VARCHAR(256),
 style_id VARCHAR(256),
-style_name VARCHAR(256))
-
+style_name VARCHAR(256),
+category_type VARCHAR(256),
+ibu_lower VARCHAR(256),
+ibu_high VARCHAR(256),
+abv_low VARCHAR(256),
+abv_high VARCHAR(256),
+co2_volume_low VARCHAR(256),
+co2_volume_high VARCHAR(256),
+lambicdegree VARCHAR(256))
--- SQL/2_CreateDW.sql
+++ SQL/2_CreateDW.sql
@@ -17,19 +17,19 @@ DROP TABLE IF EXISTS B_DW.dbo.DW_stylecategories
 DROP TABLE IF EXISTS B_DW.dbo.DW_styles
 
 CREATE TABLE B_DW.dbo.DW_beeravaliabilities(
-availability_category nvarchar(256),
-availability_id INT)
+availability_id INT,
+availability_category nvarchar(256))
 
 CREATE TABLE B_DW.dbo.DW_beers(
-abv NUMERIC(38,2),
-availability_type INT,
 beer_id INT,
 beer_name nvarchar(256),
 brewery_id INT,
 location_id INT,
+style_id INT,
+availability_type INT,
+abv NUMERIC(38,2),
 notes nvarchar(4000),
-retired INT,
-style_id INT)
+retired INT)
 
 CREATE TABLE B_DW.dbo.DW_breweries(
 brewery_id INT,
@@ -40,54 +40,53 @@ note nvarchar(4000))
 CREATE TABLE B_DW.dbo.DW_breweryservices(
 brewery_id INT,
 serving_type INT)
-
 CREATE TABLE B_DW.dbo.DW_costumers(
-email nvarchar(256),
+usr_name nvarchar(256),
 first_name nvarchar(256),
 last_name nvarchar(256),
-location_id INT,
-usr_name nvarchar(256))
+email nvarchar(256),
+location_id INT)
 
 CREATE TABLE B_DW.dbo.DW_countries(
 country_code nvarchar(256),
 country_name nvarchar(256),
-intermediate_region nvarchar(256),
 region nvarchar(256),
-sub_region nvarchar(256))
+sub_region nvarchar(256),
+intermediate_region nvarchar(256))
 
 CREATE TABLE B_DW.dbo.DW_locations(
-city nvarchar(256),
-country_code nvarchar(256),
 location_id INT,
-stat nvarchar(256))
+city nvarchar(256),
+stat nvarchar(256),
+country_code nvarchar(256))
 
 CREATE TABLE B_DW.dbo.DW_reviews(
 beer_id INT,
-feel INT,
-look INT,
-overall INT,
+usr_name nvarchar(256),
 review_date DATE,
-score INT,
+look INT,
 smell INT,
 taste INT,
-usr_name nvarchar(256))
+feel INT,
+overall INT,
+score INT)
 
 CREATE TABLE B_DW.dbo.DW_servingtypes(
-serving_category nvarchar(256),
-serving_id INT)
+serving_id INT,
+serving_category nvarchar(256))
 
 CREATE TABLE B_DW.dbo.DW_stylecategories(
 category_id INT,
 style_category nvarchar(256))
 
 CREATE TABLE B_DW.dbo.DW_styles(
-abv_high NUMERIC(38,2),
-abv_low NUMERIC(38,2),
-category_type INT,
-co2_volume_high NUMERIC(38,2),
-co2_volume_low NUMERIC(38,2),
-ibu_high INT,
-ibu_lower INT,
-lambicdegree INT,
 style_id INT,
-style_name nvarchar(256))
+style_name nvarchar(256),
+category_type INT,
+ibu_lower INT,
+ibu_high INT,
+abv_low NUMERIC(38,2),
+abv_high NUMERIC(38,2),
+co2_volume_low NUMERIC(38,2),
+co2_volume_high NUMERIC(38,2),
+lambicdegree INT)
